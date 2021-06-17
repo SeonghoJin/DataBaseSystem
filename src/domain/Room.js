7bc8576f0e779b44e721b8f6c622379703b6a75d
SeonghoJin <seongho4996@naver.com>
2021-06-17 16:35:50 +0900

    room column 변경

--- src/domain/Room.js
+++ src/domain/Room.js
@@ -1,10 +1,11 @@
 var Room = /** @class */ (function () {
     function Room(_a) {
-        var hid = _a.hid, rid = _a.rid, description = _a.description, price = _a.price;
+        var hid = _a.hid, rid = _a.rid, description = _a.description, price = _a.price, isReservation = _a.isReservation;
         this.hid = hid;
         this.rid = rid;
         this.description = description;
         this.price = price;
+        this.isReservation = isReservation;
     }
     return Room;
 }());
--- src/domain/Room.ts
+++ src/domain/Room.ts
@@ -3,17 +3,20 @@ export class Room {
     hid: number | undefined;
     rid: number | undefined;
     description: string | undefined;
-    price: number | undefined
+    price: number | undefined;
+    isReservation: boolean | undefined;
 
-    constructor({ hid, rid, description, price }: {
+    constructor({ hid, rid, description, price, isReservation }: {
         hid: number | undefined,
         rid: number | undefined,
         description: string | undefined,
-        price: number | undefined
+        price: number | undefined,
+        isReservation: boolean | undefined
     }) {
         this.hid = hid;
         this.rid = rid;
         this.description = description;
         this.price = price;
+        this.isReservation = isReservation;
     }
 }
